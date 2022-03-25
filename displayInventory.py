755ac30acd207b6219420b4c5d76fa75afee148f
Hunter Hughes <Hunter.Hughes@vu.com>
2022-03-25 11:34:54 -0500

    Changed way things print

--- displayInventory.py
+++ displayInventory.py
@@ -4,14 +4,14 @@ def displayInventory(inventory):
     print("Inventory:")
 
     #This is to re order to match desired output.
-    for key in ['apple','gold coin','rope','torch','ring']:
-        stuff[key] = stuff.pop(key)
+    #for key in ['apple','gold coin','rope','torch','ring']:
+   #     stuff[key] = stuff.pop(key)
 
     #Print the inventory
-    for itemType, item in stuff.items():
-        print(item, itemType)
+    for item in stuff:
+        print(stuff[item], item)
 
     #Print total
     print("Total number of items:", sum(stuff.values()))
 
-##displayInventory(stuff)
+displayInventory(stuff)
