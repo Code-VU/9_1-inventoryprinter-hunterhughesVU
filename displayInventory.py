9a51c242d183c9b8986b3c3b618b282b299d8888
Hunter Hughes <Hunter.Hughes@vu.com>
2022-03-25 09:38:15 -0500

    re ordered dict

--- displayInventory.py
+++ displayInventory.py
@@ -3,10 +3,15 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 def displayInventory(inventory):
     print("Inventory: ")
 
+    #This is to re order to match desired output.
+    for key in ['apple','gold coin','rope','torch','ring']:
+        stuff[key] = stuff.pop(key)
+
+    #Print the inventory
     for itemType, item in stuff.items():
         print(item, itemType)
 
-
+    #Print total
     print("Total number of items:", sum(stuff.values()))
 
 ##displayInventory(stuff)
