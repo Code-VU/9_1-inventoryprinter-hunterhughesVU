3d5a35c8f3fd9ec9b1bd2cf55958f7ac3ec8c01f
Hunter Hughes <Hunter.Hughes@vu.com>
2022-03-25 09:29:22 -0500

    initial commit

--- displayInventory.py
+++ displayInventory.py
@@ -1,6 +1,12 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
-    # your code goes here
+    print("Inventory: ")
+
+    for itemType, item in stuff.items():
+        print(item, itemType)
+
+
+    print("Total number of items:", sum(stuff.values()))
 
 ##displayInventory(stuff)
